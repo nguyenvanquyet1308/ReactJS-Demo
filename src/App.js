3f1fc578f42e382342434f250e631aa8c8b5bf53
vanquyett1308 <nguyenquyet2017zz@gmail.com>
2023-12-18 12:16:12 +0700

    nvq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Member from "./Member";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Thông tin nhân viên</p>
+      <Member name="Văn quyết" age="18"></Member>
     </div>
   );
 }
-
 export default App;
