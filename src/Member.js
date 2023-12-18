48f2ba291f1a2118e854316b2977de93f81129e4
vanquyett1308 <nguyenquyet2017zz@gmail.com>
2023-12-18 13:06:57 +0700

    nvq push

--- src/Member.js
+++ src/Member.js
@@ -1,8 +1,17 @@
 import React from "react";
+const showClick = () => {
+  alert("click làm đéo gì");
+};
 function Member({ name, age }) {
-  <div>
-    <h1>Họ và tên: {name}</h1>
-    <h2>Tuổi: {age}</h2>
-  </div>;
+  return (
+    <div>
+      <h1>Họ và tên: {name}</h1>
+      <h2>Tuổi: {age}</h2>
+      <input type="text" placeholder="Nhập vào thông tin tuổi" />
+      <button type="button" onClick={showClick}>
+        Click Me
+      </button>
+    </div>
+  );
 }
 export default Member;
